5f8c1aa7859c3f8c3d34e11cae0017d6ce0740da
Agzamxodjayev <151162119+Agzamxodjayev@users.noreply.github.com>
2024-02-22 16:20:29 +0500

    1-dars
    1-dars

---
+++ 1-dars/sin ishi/main.py
@@ -0,0 +1,46 @@
+print(".      *       .")
+print(".     * *      .")
+print(".    *   *     .")
+print(".   *     *    .")
+print(".  *       *   .")
+print(". *         *  .")
+print(".*           * .")
+
+print("")
+
+print("5 * 1 = ", 5*1)
+print("5 * 2 = ", 5*2)
+print("5 * 3 = ", 5*3)
+print("5 * 4 = ", 5*4)
+print("5 * 5 = ", 5*5)
+print("5 * 6 = ", 5*6)
+print("5 * 7 = ", 5*7)
+print("5 * 8 = ", 5*8)
+print("5 * 9 = ", 5*9)
+print("5 * 10 = ", 5*10)
+
+print("")
+
+
+print("""
+.___    *   _______
+|   |   | | |     | |
+|___|   | | |     | |
+|    |  | | |     | |
+|     | | | |     | |
+|_____| | | |_____| |
+""")
+
+print("")
+
+
+print("2009", "10", "10", sep="-")
+
+ism = input("Ismingizni kiriting")
+print(ism)
+
+yosh = input("Yoshingizni kiriting")
+print(yosh)
+
+maktab = input("Qaysi maktabda o'qishingizni kiriting")
+print(maktab)
---
+++ 1-dars/uy ishi/main.py
@@ -0,0 +1,6 @@
+print("""
+Ismim: Bilolxo'ja;
+Familiyam: Agzamxodjayev;
+Yoshim: 12 da;""")
+print("Men dasturchi bo'lganimda 16 yoshlar atrofida bo'laman.")
+print("Men bo'sh vaqtlarimda ko'proq IT bilan shug'illanaman.")
